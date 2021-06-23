688ffb8a3419232a5abbd1c86df575b4e31cbe2f
exmpemajl <cciobanu5151@gmail.com>
2021-06-23 22:31:32 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -89,7 +89,7 @@ const startGame = () => {
         enemy.y = -100 * setting.traffic * (i + 1);
         enemy.style.left = Math.floor(Math.random() * (gameArea.offsetWidth - 50)) + "px";
         enemy.style.top = enemy.y + "px";
-        enemy.style.background = "transparent url('../img/enemy2.png') center / cover no-repeat";
+        enemy.style.background = "transparent url('./img/enemy2.png') center / cover no-repeat";
         gameArea.appendChild(enemy);
     }
 
